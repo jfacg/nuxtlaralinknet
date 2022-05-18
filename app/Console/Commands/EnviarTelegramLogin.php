76772cebde0db8b9aab0f43ce8191a0442dec597
Jailson Araujo <jailson.pb@gmail.com>
2022-05-18 15:28:12 -0300

    offline

--- app/Console/Commands/EnviarTelegramLogin.php
+++ app/Console/Commands/EnviarTelegramLogin.php
@@ -3,7 +3,7 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
-use App\Models\Telegram\AlertTeste;
+use App\Models\Telegram\AlertOffline;
 use App\Http\Controllers\Api\ClientIxcController;
 use App\Models\Clientixc;
 use App\Models\Loginixc;
@@ -61,29 +61,52 @@ class EnviarTelegramLogin extends Command
         Offline::create($quantidade);
 
         $dif = $offatual - $offanterior->quantidade;
-        AlertTeste::enviarMensagem("Anterior: ".$offanterior->quantidade."\nAtual: ".$offatual."\nDiferenca: ".$dif);
 
-        if ($dif < -4) {
-            AlertTeste::enviarMensagem("###### EVENTO RESOLVIDO ######");
+        if ($dif > 0) {
+            AlertOffline::enviarMensagem("Anterior: ".$offanterior->quantidade."\nAtual: ".$offatual."\nDesconectados: ".$dif);
         }
 
-        if ($dif > 4) {
-            $clientes = [];
-            
-            $logins = Loginixc::where('online', '=', 'N')
-                        ->orderBy('ultima_conexao_final', 'desc')
-                        ->get(['id', 'id_cliente', 'senha', 'login', 'mac', 'onu_mac', 'ultima_conexao_final']);
+        if ($dif < 0) {
+            AlertOffline::enviarMensagem("Anterior: ".$offanterior->quantidade."\nAtual: ".$offatual."\nConectados: ".$dif);
+        }
 
+        if ($dif < -4) {
+            $logins = Loginixc::where('online', '=', 'S')
+                        ->orderBy('ultima_conexao_inicial', 'asc')
+                        ->limit($dif)
+                        ->get(['id', 'id_cliente', 'senha', 'login', 'mac', 'onu_mac', 'ultima_conexao_inicial']);
+
+            $clientes = [];
             for ($i=0; $i < $dif; $i++) { 
                 $cliente = Clientixc::where('id', '=', $logins[$i]->id_cliente)->first();
+                $cliente->login = $logins[$i]->login;
                 array_push($clientes, $cliente);
             }
             $lista = '';
             foreach ($clientes as $cliente) {
-                $lista = $lista."\n"."\n".$cliente->razao."\n".$cliente->endereco;
+                $lista = $lista."\n"."\n".$cliente->razao."\n".$cliente->endereco."\n".$cliente->login;
             }
 
-            AlertTeste::enviarMensagem($lista);
+            AlertOffline::enviarMensagem("###### EVENTO FINALIZADO ######"."\n \n".$lista);
+        }
+
+        if ($dif > 4) {
+            $logins = Loginixc::where('online', '=', 'N')
+                        ->orderBy('ultima_conexao_final', 'desc')
+                        ->get(['id', 'id_cliente', 'senha', 'login', 'mac', 'onu_mac', 'ultima_conexao_final']);
+
+            $clientes = [];
+            for ($i=0; $i < $dif; $i++) { 
+                $cliente = Clientixc::where('id', '=', $logins[$i]->id_cliente)->first();
+                $cliente->login = $logins[$i]->login;
+                array_push($clientes, $cliente);
+            }
+            $lista = '';
+            foreach ($clientes as $cliente) {
+                $lista = $lista."\n"."\n".$cliente->razao."\n".$cliente->endereco."\n".$cliente->login;
+            }
+
+            AlertOffline::enviarMensagem("###### EVENTO INICIADO ######"."\n \n".$lista);
         }
 
 
